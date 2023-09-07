8a981c52829f32ddd78e006c5ca6c5cfa283ed75
JonasADM <jonassilvadelara@gmail.com>
2023-09-07 00:26:54 -0300

    commit teste

--- listaDuplamenteEncadeada.c
+++ listaDuplamenteEncadeada.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*lista duplamente encadeada com funções para adicionar elementos, 
+buscar na lista, imprimir na ordem normal ou inversa, remover elementos
+da lista, concatenar 2 listas, copiar uma lista encadeada para um ponteiro*/
+Struct dados{
+	int value;
+	struct dados* next;
+	struct dados* ant;
+};
+typedef struct dados dados;
 int main(){
 	return 0;
 }
